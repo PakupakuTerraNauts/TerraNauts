e63b6f2fa67c5a08984eda259be4f109c77e9371
st20153yr <st20153yr@gm.ibaraki-ct.ac.jp>
2023-07-09 00:35:42 +0900

    戻った

--- Assets/Scenes/changeToRestroom.cs
+++ Assets/Scenes/changeToRestroom.cs
@@ -1,17 +1,14 @@
-using UnityEngine;
-using UnityEngine.SceneManagement;
-
-public class Scene_change : MonoBehaviour 
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;using UnityEngine.SceneManagement;
+ 
+public class Test : MonoBehaviour
 {
  
-    private void OnTriggerStay2D(Collider2D other)
-    {   
-        if (other.gameObject.CompareTag("Player"))
-        {
-            if(Input.GetKey (KeyCode.UpArrow))
-            {
-                SceneManager.LoadScene("restroom");
-            }
-        }   
+    void OnTriggerStay2D(Collider2D other)
+    {
+        if (Input.GetKey(KeyCode.UpArrow)){
+            SceneManager.LoadScene("restroom");
+        }
     }
 }
