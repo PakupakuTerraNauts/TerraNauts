95be3d90ede7b9a563a616cb39b46d7293de9609
のるん <97i.issei@gmail.com>
2023-07-25 15:57:01 +0900

    stage1にUI表示

--- Assets/Script/Status/StatusManager.cs
+++ Assets/Script/Status/StatusManager.cs
@@ -24,27 +24,27 @@ public class StatusManager : MonoBehaviour
     void Update()
     {
         Text HPText = HPObject.GetComponent<Text>();
-        string HP_S = Status.HP.ToString("d");
+        string HP_S = Player.HP.ToString("d");
         HPText.text = HP_S;
 
         Text ATKText = ATKObject.GetComponent<Text>();
-        string ATK_S = Status.ATK.ToString("d");
+        string ATK_S = Player.ATK.ToString("d");
         ATKText.text = ATK_S;
 
         Text DEFText = DEFObject.GetComponent<Text>();
-        string DEF_S = Status.DEF.ToString("d");
+        string DEF_S = Player.DEF.ToString("d");
         DEFText.text = DEF_S;
 
         Text SPDText = SPDObject.GetComponent<Text>();
-        string SPD_S = Status.SPD.ToString("d");
+        string SPD_S = Player.SPD.ToString("d");
         SPDText.text = SPD_S;
 
         Text CRITRATEText = CRITRATEObject.GetComponent<Text>();
-        string CRITRATE_S = Status.CRITRATE.ToString("d");
+        string CRITRATE_S = Player.CRITRATE.ToString("d");
         CRITRATEText.text = CRITRATE_S;
 
         Text CRITDMGText = CRITDMGObject.GetComponent<Text>();
-        string CRITDMG_S = Status.CRITDMG.ToString("d");
+        string CRITDMG_S = Player.CRITDMG.ToString("d");
         CRITDMGText.text = CRITDMG_S;
 
     }
