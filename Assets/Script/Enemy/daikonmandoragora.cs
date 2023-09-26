140b6629d94cdbd2a1f25e41a04bd77874a58ae4
AigaNS <st20134ng@gm.ibaraki-ct.ac.jp>
2023-09-26 13:18:40 +0900

    大根は殺せる。スライム途中。

--- Assets/Script/Enemy/daikonmandoragora.cs
+++ Assets/Script/Enemy/daikonmandoragora.cs
@@ -9,44 +9,37 @@ public class daikonmandoragora : MonoBehaviour
     public float gravity;
     public HPBar HP;
 
-    private float hp = 10.0f;
+    private float hp = 0.0f;
     private bool isDead = false;
     private bool isFumareta = false;
     private SpriteRenderer sr = null;
     private Rigidbody2D rb = null;
     private ObjectCollision oc = null;
     private Animator anim = null;
-    private BoxCollider2D col = null;
+    private CapsuleCollider2D capcol = null;
     private string playerTag = "Player";
     private string swordTag = "Sword";
     #endregion
 
-    void Awake()
+    void Start()
     {
         sr = GetComponent<SpriteRenderer>();
         rb = GetComponent<Rigidbody2D>();
         anim = GetComponent<Animator>();
         oc = GetComponent<ObjectCollision>();
-        col = GetComponent<BoxCollider2D>();
+        capcol = GetComponent<CapsuleCollider2D>();
+
+        hp = HPBar.instance.currentHealth;
     }
 
-    void Update()
-    {
-        hp = HPBar.instance.currentHealth;
-        if(hp <= 0.0f){
-            anim.Play("radissh_die");
-            isDead = true;
-            col.enabled = false;
-            Destroy(gameObject, 3f);
-        }
-        
-    }
+    
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
         if(collision.collider.tag == playerTag && !isFumareta){
             anim.Play("radissh_fumare");
             isFumareta = true;
+            capcol.tag = "Enemy";  //enemy ja naku sitoite.
         }
     }
 
@@ -54,6 +47,14 @@ public class daikonmandoragora : MonoBehaviour
     {
         if(collision.tag == swordTag && !isDead){
             HP.UpdateHP(10.0f); // player kara kougekiryoku wo syutoku suru.
+            hp -= 10.0f;
+        }
+
+        if(hp <= 0.0f){
+            anim.Play("radissh_die");
+            isDead = true;
+            capcol.enabled = false;
+            Destroy(gameObject, 3f);
         }
     }
 }
--- Assets/Script/Enemy/suraimu.cs
+++ Assets/Script/Enemy/suraimu.cs
@@ -4,25 +4,45 @@ using UnityEngine;
 
 public class suraimu : MonoBehaviour
 {
-    private float HP = 70;
+    private HPBar HP;
+
+    private float hp = 0.0f;
     private bool isDead = false;
     private Animator anim = null;
-    private CircleCollider2D col = null;
+    private CircleCollider2D circol = null;
     private Rigidbody2D rb = null;
     private SpriteRenderer sr = null;
+    private string swordTag = "Sword";
 
     void Start(){
         anim = GetComponent<Animator>();
-        col = GetComponent<CircleCollider2D>();
+        circol = GetComponent<CircleCollider2D>();
         rb = GetComponent<Rigidbody2D>();
         sr = GetComponent<SpriteRenderer>();
+
+        hp = HPBar.instance.currentHealth;
     }
 
     void Update(){
         if(sr.isVisible){   //‰æ–ÊŠO‚Å‚Í“®‚©‚È‚¢
-
+            anim.Play("suraimu_furueru");
         } else {
             rb.Sleep();
         }
-    } 
+    }
+
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        if(collision.tag == swordTag){
+            HP.UpdateHP(10.0f);     //player no kougekiryoku wo tukutte koko ni ireru.
+            hp -= 10.0f;            //koko mo kougekiryoku.
+        }
+        
+        if(hp <= 0.0f){
+            anim.Play("suraimu_die");
+            isDead = true;
+            circol.enabled = false;
+            Destroy(gameObject, 3f);
+        }
+    }
 }
